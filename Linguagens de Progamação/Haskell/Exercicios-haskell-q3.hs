8a63dcf30416752a9f67d7e459b39c6e7cc46f7f
henriquefont <henriquefontoura.exe@gmail.com>
2023-05-22 09:11:18 -0300

    last commit

--- Linguagens de Progamação/Haskell/Exercicios-haskell-q3.hs
+++
@@ -1,9 +0,0 @@
-main :: IO ()
-parouimpar :: Int -> String
-parouimpar n
-  | n `rem` 2 == 0 = "O numero e par."
-  | otherwise = "O numero e impar."
-main = do
-  putStrLn "\nDigite um numero."
-  n <- readLn
-  putStrLn ("\n" ++ show (parouimpar n))
--- Linguagens de Progamação/Haskell/Exercícios-de-Recursão-Haskell-q2.hs
+++
@@ -1,9 +0,0 @@
-main :: IO ()
-numDigito :: Float -> Float
-numDigito n
-  | n < 10 && n >= 0 = 1
-  | otherwise = 1 + numDigito (n / 10)
-main = do
-  putStrLn ("\nDigite um numero.")
-  n <- readLn
-  putStrLn ("\nQuantidade de digitos: " ++ show (numDigito n))
