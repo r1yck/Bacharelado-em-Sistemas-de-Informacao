8dd91a38d8e2ff77569b7ee335a85526194030a5
Henrique Fontoura <121917557+r1yck@users.noreply.github.com>
2023-05-22 08:42:52 -0300

    Delete Linguagens de Progamação directory

--- Linguagens de Progamação/Haskell/tarefa2-q2.hs
+++
@@ -1,7 +0,0 @@
-main :: IO ()
-f :: Float -> Float
-f c = (c * 1.8) + 32
-main = do
-  putStrLn ("\nDigite o valor em graus (C).")
-  c <- readLn
-  putStrLn ("\nValor em Farenheit (F): " ++ show (f c))
--- Linguagens de Progamação/Prolog/Exercicios_Prolog_2-q8.pl
+++
@@ -1,8 +0,0 @@
-come(abelha, polen).
-come(passaro, abelha).
-come(cobra, passaro).
-come(raposa, cobra).
-
-presa(Y, X) :- come(X, Y).
-
-predador(X, Y) :- come(X, Y).
