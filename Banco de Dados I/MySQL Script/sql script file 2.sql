42a691e15fbfb879f20373a26c9ea3c4343be974
Henrique Fontoura <henriquefontoura.exe@gmail.com>
2023-10-20 11:41:04 -0300

    Commit das primeiras atividades de sql script

---
+++ Banco de Dados I/MySQL Script/sql script file 2.sql
@@ -0,0 +1,34 @@
+-- create database Farmacia;
+use Farmacia;
+
+/*create table medicamento (
+nomeVenda varchar(45),
+principioAtivo varchar(45),
+
+primary key (nomeVenda)
+);
+
+create table virus (
+nomeCientifico varchar(45),
+nomePopular varchar(45),
+
+primary key (nomeCientifico)
+);
+
+create table paciente5 (
+idPaciente int auto_increment,
+nome varchar(45),
+
+primary key (idPaciente)
+);*/
+
+create table trata8 (
+nomeVenda varchar(45),
+nomeCientifico varchar(45),
+idPaciente int,
+
+primary key (nomeVenda, nomeCientifico, idPaciente),
+foreign key (nomeVenda) references medicamento (nomeVenda),
+foreign key (nomeCientifico) references virus (nomeCientifico),
+foreign key (idPaciente) references paciente5 (idPaciente)
+);
